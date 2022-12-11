9fcf4035949ff616bd457356eb8b0de34ccf0995
Kirill <bloodwingest@gmail.com>
2022-12-11 22:02:25 +0300

    feat: add settings to ts, eslint and webpack

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -15,7 +17,7 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js']
     },
     output: {
         filename: 'index.js',
@@ -27,6 +29,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' })
     ],
 };
 
