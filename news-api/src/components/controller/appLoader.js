2d0072d5f8027719ecc9c3487a50d5c642ad3a90
Kirill <bloodwingest@gmail.com>
2022-12-11 21:55:13 +0300

    feat: add packages and api key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ffa29b570b1848d08bf4e3bd3eda1a12', // получите свой ключ https://newsapi.org/
         });
     }
 }
