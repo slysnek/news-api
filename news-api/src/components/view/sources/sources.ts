fc1fbf1ba9612343bd10e8409864e39e29ac5dcd
Kirill <bloodwingest@gmail.com>
2022-12-14 20:39:20 +0300

    feat: add character filtration

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -2,18 +2,50 @@ import './sources.css';
 import { SourcesData } from '../../../types/types';
 
 class Sources {
-    draw(data: SourcesData[]) {
+    draw(data: SourcesData[], char?: string[]) {
         const fragment = document.createDocumentFragment();
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
-        data.forEach((item: SourcesData) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
-            const sourceItemName = sourceClone.querySelector('.source__item-name') as HTMLTemplateElement;
-            sourceItemName.textContent = item.name;
-            const sourceItem = sourceClone.querySelector('.source__item') as HTMLTemplateElement;
-            sourceItem.setAttribute('data-source-id', item.id);
-            fragment.append(sourceClone);
+
+        const alphabetCharacters = document.querySelectorAll('.characters');
+        alphabetCharacters.forEach((value, index) => {
+            value.addEventListener('click', () => {
+                const charSelector: string[][] = [
+                    ['A', 'B', 'C', 'D', 'E'],
+                    ['F', 'G', 'H', 'I', 'K'],
+                    ['J', 'L', 'M', 'N', 'O'],
+                    ['P', 'Q', 'R', 'S', 'T'],
+                    ['U', 'V', 'W', 'X', 'Y', 'Z'],
+                ];
+                this.draw(data, charSelector[index]);
+            });
         });
+
         const sourcesWrap = document.querySelector('.sources') as HTMLTemplateElement;
+
+        data.forEach((item: SourcesData) => {
+            if (char) {
+                sourcesWrap.innerHTML = '';
+                char.forEach((value) => {
+                    if (value === item.name.toUpperCase().split('')[0]) {
+                        const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
+                        const sourceItemName = sourceClone.querySelector('.source__item-name') as HTMLTemplateElement;
+                        sourceItemName.textContent = item.name;
+                        const sourceItem = sourceClone.querySelector('.source__item') as HTMLTemplateElement;
+                        sourceItem.setAttribute('data-source-id', item.id);
+                        fragment.append(sourceClone);
+                    }
+                });
+            } else {
+                //dry principle is silently smoking and crying
+                const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
+                const sourceItemName = sourceClone.querySelector('.source__item-name') as HTMLTemplateElement;
+                sourceItemName.textContent = item.name;
+                const sourceItem = sourceClone.querySelector('.source__item') as HTMLTemplateElement;
+                sourceItem.setAttribute('data-source-id', item.id);
+                fragment.append(sourceClone);
+            }
+        });
+
         sourcesWrap.append(fragment);
     }
 }
